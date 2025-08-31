6241a8ca56a6e2373d6d8b4b73fd8aece02fd801
harvey <1031964705@qq.com>
2025-08-31 22:31:22 +0800

    载入音频文件

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 ﻿#define SDL_MAIN_HANDLED
 #include <iostream>
+#include <filesystem>
 #include <SDL.h>
 #include "main.h"
 int main(int, char const **)
 {
     int retval = 0;
     std::cout << "Hello,SpaceShoot." << std::endl;
-
+    /* 获取当前工作区路径*/
+    std::filesystem::path cwd = std::filesystem::current_path();
     /* 初始化SDL*/
     retval = SDL_Init(SDL_INIT_EVERYTHING);
     if (retval != 0)
@@ -18,7 +20,14 @@ int main(int, char const **)
     retval = IMG_Init(SPACESHOOT_IMG_FORMAT);
     if (retval != SPACESHOOT_IMG_FORMAT)
     {
-        std::cout << "SDL_IMG Init failed: " << IMG_GetError() << std::endl;
+        std::cout << "SDL_IMG Init failed: " << SDL_GetError() << std::endl;
+        return -1;
+    }
+    /* SDL_mixer初始化*/
+    retval = Mix_OpenAudio(SPACESHOOT_MIX_FREQUENCY, SPACESHOOT_MIX_FORMAT, SPACESHOOT_MIX_CHANNELS, SPACESHOOT_MIX_BUFFER_SIZE);
+    if (retval != 0)
+    {
+        std::cout << "SDL_IMG Init failed: " << SDL_GetError() << std::endl;
         return -1;
     }
     /* 创建SDL窗口*/
@@ -36,12 +45,23 @@ int main(int, char const **)
         return -1;
     }
     /* 创建图片纹理*/
-    SDL_Texture *texture = IMG_LoadTexture(renderer, "../assets/image/bg.png");
+    std::filesystem::path img_bg_path = cwd / "assets" / "image" / "bg.png";
+    SDL_Texture *texture = IMG_LoadTexture(renderer, img_bg_path.string().c_str());
     if (texture == nullptr)
     {
         std::cout << "Load image failed: " << SDL_GetError() << std::endl;
         return -1;
     }
+    /* 创建音频对象*/
+    std::filesystem::path mix_bg_path = cwd / "assets" / "music" / "03_Racing_Through_Asteroids_Loop.ogg";
+    Mix_Music *music = Mix_LoadMUS(mix_bg_path.string().c_str());
+    if (music == nullptr)
+    {
+        std::cout << "Load mix failed: " << SDL_GetError() << std::endl;
+        return -1;
+    }
+    /* 播放音频*/
+    Mix_PlayMusic(music, -1);
 
     while (SDL_TRUE)
     {
@@ -74,10 +94,19 @@ int main(int, char const **)
         SDL_RenderPresent(renderer);
     }
 
+    /* 销毁音频对象*/
+    Mix_FreeMusic(music);
+    Mix_CloseAudio();
+    /* 销毁图片纹理*/
+    SDL_DestroyTexture(texture);
     /* 销毁渲染器*/
     SDL_DestroyRenderer(renderer);
     /* 销毁窗口*/
     SDL_DestroyWindow(window);
+    /* 退出SDL_mixer*/
+    Mix_Quit();
+    /* 退出SDL_image*/
+    IMG_Quit();
     /* 退出SDL*/
     SDL_Quit();
     
--- main.h
+++ main.h
@@ -1,5 +1,11 @@
-#ifndef __MAIN_H__
+﻿#ifndef __MAIN_H__
 #define __MAIN_H__
 #include <SDL_image.h>
-#define SPACESHOOT_IMG_FORMAT   (IMG_INIT_JPG | IMG_INIT_PNG)
+#include <SDL_mixer.h>
+#define SPACESHOOT_IMG_FORMAT (IMG_INIT_JPG | IMG_INIT_PNG) /* 图片格式*/
+#define SPACESHOOT_MIX_FREQUENCY MIX_DEFAULT_FREQUENCY      /* 音频频率*/
+#define SPACESHOOT_MIX_FORMAT MIX_DEFAULT_FORMAT            /* 音频格式*/
+#define SPACESHOOT_MIX_CHANNELS MIX_DEFAULT_CHANNELS        /* 音频通道*/
+#define SPACESHOOT_MIX_VOLUME MIX_MAX_VOLUME                /* 音频音量*/
+#define SPACESHOOT_MIX_BUFFER_SIZE 2048                     /* 音频输出缓冲区大小*/
 #endif
